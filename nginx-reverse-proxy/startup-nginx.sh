dc4c4361afa82ca2b7c76a9cbfa9e54d3b7daed5
zabenno <benc853@gmail.com>
2020-04-18 00:07:38 +1200

    Removed bad option.

--- nginx-reverse-proxy/startup-nginx.sh
+++ nginx-reverse-proxy/startup-nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i "s/FQDN/$FQDN_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
-sed -i "s/SERVER_NAME/$SERVER_NAME_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
-sed -i "s/SERVER_PORT/$SERVER_PORT_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
-sed -i "s/CRT_FILE/$CRT_FILE_PATH/g" /etc/nginx/sites-available/reverse-proxy.conf
-sed -i "s/KEY_FILE/$KEY_FILE_PATH/g" /etc/nginx/sites-available/reverse-proxy.conf
+sed -i "/FQDN/$FQDN_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
+sed -i "/SERVER_NAME/$SERVER_NAME_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
+sed -i "/SERVER_PORT/$SERVER_PORT_FILE/g" /etc/nginx/sites-available/reverse-proxy.conf
+sed -i "/CRT_FILE/$CRT_FILE_PATH/g" /etc/nginx/sites-available/reverse-proxy.conf
+sed -i "/KEY_FILE/$KEY_FILE_PATH/g" /etc/nginx/sites-available/reverse-proxy.conf
 
 /usr/bin/nginx
