8888504eeb352a0cda4d8b54bf7c0d28f2df406a
zabenno <benc853@gmail.com>
2020-04-18 16:17:32 +1200

    Changed to env var for non sensitive data.

--- nginx-reverse-proxy/startup-nginx.sh
+++ nginx-reverse-proxy/startup-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed -i 's/FQDN/'"$(cat $FQDN_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/SERVER_NAME/'"$(cat $SERVER_NAME_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/SERVER_PORT/'"$(cat $SERVER_PORT_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/FQDN/'"$FQDN"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/SERVER_NAME/'"$SERVER_NAME"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/SERVER_PORT/'"$SERVER_PORT"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/CRT_FILE/'"$(cat $CRT_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/KEY_FILE/'"$(cat $KEY_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 
