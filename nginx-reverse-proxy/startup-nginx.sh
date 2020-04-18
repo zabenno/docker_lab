13c8a526959ad30772345f31850ba42a0210c24b
zabenno <benc853@gmail.com>
2020-04-18 16:57:48 +1200

    Changed sed seperator due to use of file path.

--- nginx-reverse-proxy/startup-nginx.sh
+++ nginx-reverse-proxy/startup-nginx.sh
@@ -2,8 +2,8 @@
 sed -i 's/FQDN/'"$FQDN"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/SERVER_NAME/'"$SERVER_NAME"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/SERVER_PORT/'"$SERVER_PORT"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/CRT_FILE/'"$CRT_FILE_PATH"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/KEY_FILE/'"$KEY_FILE_PATH"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's+CRT_FILE+'"$CRT_FILE_PATH"'+g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's+KEY_FILE+'"$KEY_FILE_PATH"'+g' /etc/nginx/sites-available/reverse-proxy.conf
 
 echo "------------------------------------------------\nUsing nginx configuration:\n"
 cat /etc/nginx/sites-available/reverse-proxy.conf
