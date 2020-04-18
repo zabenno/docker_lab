6a06d947c291eb153b2d5402385a4d832b08b5b5
zabenno <benc853@gmail.com>
2020-04-18 16:42:48 +1200

    Fixed logic flaw.

--- nginx-reverse-proxy/startup-nginx.sh
+++ nginx-reverse-proxy/startup-nginx.sh
@@ -2,7 +2,7 @@
 sed -i 's/FQDN/'"$FQDN"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/SERVER_NAME/'"$SERVER_NAME"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/SERVER_PORT/'"$SERVER_PORT"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/CRT_FILE/'"$(cat $CRT_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/KEY_FILE/'"$(cat $KEY_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/CRT_FILE/'"$CRT_FILE_PATH"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/KEY_FILE/'"$KEY_FILE_PATH"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 
 nginx
