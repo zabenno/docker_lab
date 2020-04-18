818aff5562f3a59d1bc9ced559e92fd1797dd879
zabenno <benc853@gmail.com>
2020-04-18 15:58:05 +1200

    Fixed typo.

--- nginx-reverse-proxy/startup-nginx.sh
+++ nginx-reverse-proxy/startup-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's/FQDN/'"$(cat $FQDN_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/SERVER_NAME/'"$(cat $SERVER_NAME_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
-sed -i 's/SERVER_PORT/'"$(cat SERVER_PORT_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
+sed -i 's/SERVER_PORT/'"$(cat $SERVER_PORT_FILE)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/CRT_FILE/'"$(cat $CRT_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 sed -i 's/KEY_FILE/'"$(cat $KEY_FILE_PATH)"'/g' /etc/nginx/sites-available/reverse-proxy.conf
 
